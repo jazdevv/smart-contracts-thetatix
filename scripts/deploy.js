a709177201a91b6c75774ce792b7ea8b5afbb08c
Jazthedev <joelpepa2@gmail.com>
2023-05-05 19:01:19 +0200

    add deployment for theta network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,17 +10,18 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const lockedAmount = hre.ethers.utils.parseEther("0.00001");
+  
+  const Lock = await hre.ethers.getContractFactory("ticketsFactory");
+  const lock = await Lock.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
   await lock.deployed();
 
   console.log(
     `Lock with ${ethers.utils.formatEther(
       lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    )}TFUEL and unlock timestamp ${unlockTime} and contract address: ${lock.address}`
   );
 }
 
